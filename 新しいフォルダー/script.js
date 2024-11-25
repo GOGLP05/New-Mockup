76dfe563b5a5fa8945fd7cc1ae5d7aeb18a5869d
GOGLP05 <23JN0338@jec.ac.jp>
2024-11-25 10:11:15 +0900

    Delete 新しいフォルダー directory

--- 新しいフォルダー/script.js
+++
@@ -1,7 +0,0 @@
-function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    const mainContent = document.querySelector('.main-content');
-    
-    sidebar.classList.toggle('hidden');
-    mainContent.classList.toggle('shifted');
-}
