68621eee8cc65f774adc35388c73fda2aca495da
宮内慎司 <23jn0338@jec.ac.jp>
2024-12-19 13:32:00 +0900

    ログイン

--- User/login.php
+++ User/login.php
@@ -1,42 +1,5 @@
 
 
-<?php
-/*
-session_start();
-
-$host = "10.32.97.1/sotu";
-$dbname = "UserTable";
-$username = "23jn03_G5";
-$password = "23jn03_G5";
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("データベース接続失敗: " . $e->getMessage());
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST["admin_id"];
-    $user_password = $_POST["password"];
-    
-    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
-    $stmt->bindParam(':email', $email);
-    $stmt->execute();
-    
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-    
-    if ($user && password_verify($user_password, $user['password'])) {
-        $_SESSION['user_id'] = $user['id'];
-        $_SESSION['email'] = $user['email'];
-        header("Location: top.php"); 
-        exit;
-    } else {
-        $error_message = "メールアドレスまたはパスワードが間違っています。";
-    }
-}
-    */
-?>
 
 
 
